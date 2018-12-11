d16d9a692c45ed9aa9b45c22ae3828b8720bb46b
Jeyaraman <rjayavrp2@gmail.com>
2018-12-11 16:43:25 +0000

    Modified Script to read more than one line from sensorlog file

--- web.js
+++ web.js
@@ -9,30 +9,50 @@ function handle_incoming_request (req, res) {
 		if (err) { 
 			console.log("Couldn't read file");
 		}
-		var array = readings;
+		var lines = readings;
+		//console.log(lines);
+		lines = lines.toString('ascii', 0, lines.length);
+		lines = lines.split("\n");
+
+		var z;
+		for(z in lines) {
+		console.log(lines[z]);
+		lines[z]=lines[z].replace(/(\r\n|\n|\r)/gm,"");
+		}
+        var topobj = {};
+        var key = 'Sensor Log';
+        topobj[key]=[];
+		///////////////////
+		for(z =0; z < lines.length; z++) {
+
+		var array = lines[z];
+		//console.log(array);
+		//console.log(lines.length.toString());
 		array = array.toString('ascii', 0, array.length);
 		array = array.split(",");
-	    for(i in array) {
-			console.log(array[i]);
-		}
+	    //for(i in array) {
+		//	console.log(array[i]);
+		//}
 		var text1 = '{"temperature":'+'"'+array[0]+'",';
 		var text2 = '"humidity":'+'"'+array[1]+'",';
 		var text3 = '"wind speed":'+'"'+array[2]+'",';
 		var text4 = '"time":'+'"'+array[3]+'",';
 		var text5 = '"location":'+'"'+array[4]+'"}';
 
-		var sensorText = text1 + text2 + text3 + text4 + text5;
-    
+		sensorText = text1 + text2 + text3 + text4 + text5;   	
+		console.log(sensorText);
 		var obj = JSON.parse(sensorText);
+        topobj[key].push(obj);
+		}
 
 		res.writeHead(200, { "Content-Type" : "application/json" });
-		res.end(JSON.stringify(obj));
-
+		res.end(JSON.stringify(topobj));
    });
 }
 
 function load_sensor_data(callback) {
-   fs.readFile(
+  	
+      fs.readFile(
 		"sensorlog.txt",'utf8',
 		function (err, readings) {
 
@@ -41,7 +61,7 @@ function load_sensor_data(callback) {
 			return;
 		}
 		callback(null, readings);
-	});
+	});   
 }
 
 var s = http.createServer(handle_incoming_request);
